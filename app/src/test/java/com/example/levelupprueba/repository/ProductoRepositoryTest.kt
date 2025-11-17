f0c1443f280bd628d93c3340bde6fe1b730538cb
ariel <ari.molinam@duocuc.cl>
2025-11-17 00:38:05 -0300

    - Nuevos tests para ProductoRepositoryTest

--- app/src/test/java/com/example/levelupprueba/repository/ProductoRepositoryTest.kt
+++ app/src/test/java/com/example/levelupprueba/repository/ProductoRepositoryTest.kt
@@ -1,5 +1,7 @@
 import com.example.levelupprueba.data.repository.ProductoRepository
 import com.example.levelupprueba.model.producto.Producto
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -41,4 +43,69 @@ class ProductoRepositoryTest {
         // Verifica que no haya ids repetidos
         assertEquals(ids.size, ids.toSet().size)
     }
+
+    @Test
+    fun `obtenerProductoPorId devuelve null cuando el id no existe`() {
+
+        val resultado = repository.obtenerProductoPorId("ID_INEXISTENTE")
+
+        assertNull(resultado)
+    }
+
+    @Test
+    fun `obtenerProductoPorId devuelve el producto correcto cuando el id existe`() {
+        // Id real desde el repositorio
+        val primerProducto = repository.obtenerProductos().first()
+        val idBuscado = primerProducto.id
+
+        // Act
+        val resultado = repository.obtenerProductoPorId(idBuscado)
+
+        // Assert
+        assertNotNull(resultado)
+        assertEquals(idBuscado, resultado?.id)
+    }
+
+    @Test
+    fun `obtenerProductosDestacados devuelve solo productos destacados`() {
+        val destacados = repository.obtenerProductosDestacados()
+
+        assertTrue(destacados.all { it.destacado })
+    }
+
+    @Test
+    fun `obtenerReviews retorna una lista valida para un id existente`() = runTest {
+        val producto = repository.obtenerProductos().first()
+
+        val reviews = repository.obtenerReviews(producto.id)
+
+        assertNotNull(reviews)
+        assertTrue(reviews.isNotEmpty() || reviews.isEmpty())
+    }
+
+    @Test
+    fun `obtenerReviews retorna reviews si el producto tiene reviews`() = runTest {
+        val productoConReviews = repository
+            .obtenerProductos()
+            .firstOrNull { it.reviews.isNotEmpty() }
+
+        if (productoConReviews != null) {
+            val resultado = repository.obtenerReviews(productoConReviews.id)
+
+            assertTrue(resultado.isNotEmpty())
+        }
+    }
+    @Test
+    fun `obtenerReviews devuelve lista mock no vacia cuando no hay reviewDao`() = runTest {
+        val producto = repository.obtenerProductos().first()
+        val productoId = producto.id
+
+        // Act
+        val reviews = repository.obtenerReviews(productoId)
+
+        assertNotNull(reviews)
+        assertTrue(reviews.isNotEmpty())
+        assertTrue(reviews.all { it.productoId == productoId })
+    }
+
 }
